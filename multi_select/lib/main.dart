1f0e264d67037c8ad4063d7b4559f5cdfa6a839e
Gideon Acromond <acromondx@gmail.com>
2023-03-13 09:47:38 +0000

    boom

--- multi_select/lib/main.dart
+++ multi_select/lib/main.dart
@@ -1,115 +1,156 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const MultiSelect());
+
+class MultiSelect extends StatefulWidget {
+  const MultiSelect({super.key});
+
+  @override
+  State<MultiSelect> createState() => _MultiSelectState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MultiSelectState extends State<MultiSelect> {
+  existsInTrashCan(Course course) => trashCan.contains(course);
+  List<Course> trashCan = [];
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.cyan.shade900,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      home: Scaffold(
+        appBar: trashCan.isEmpty
+            ? AppBar(
+                title: const Text('Multi-Delete'),
+                actions: const [],
+                centerTitle: true,
+                backgroundColor: Colors.grey.shade900,
+              )
+            : AppBar(
+                backgroundColor: Colors.grey.shade800,
+                leading: IconButton(
+                  icon: const Icon(Icons.clear),
+                  onPressed: () {
+                    setState(() {
+                      trashCan.clear();
+                    });
+                  },
+                ),
+                title: Text(trashCan.length.toString()),
+                actions: [
+                  IconButton(
+                      onPressed: () {
+                        for (var trashItem in trashCan) {
+                          coursesData.removeWhere(
+                              (course) => course.id == trashItem.id);
+                          trashCan.clear();
+                          setState(() {});
+                        }
+                      },
+                      icon: const Icon(Icons.delete))
+                ],
+              ),
+        body: ListView(
+          children: [
+            const SizedBox(height: 20),
+            const SizedBox(height: 10),
+            ListView.separated(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: coursesData.length,
+              separatorBuilder: (context, index) => const SizedBox(height: 5),
+              itemBuilder: (BuildContext context, int index) {
+                return PrettyCard(
+                  name: coursesData[index].name,
+                  isSelected: existsInTrashCan(coursesData[index]),
+                  trashCan: trashCan,
+                  onTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('Single Tap!'),
+                      duration: Duration(seconds: 1),
+                    ));
+                  },
+                  onDelete: () {
+                    if (trashCan.contains(coursesData[index])) {
+                      trashCan.remove(coursesData[index]);
+                      setState(() {});
+                    } else {
+                      trashCan.add(coursesData[index]);
+                      setState(() {});
+                    }
+                  },
+                );
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class PrettyCard extends StatelessWidget {
+  final String name;
+  final bool isSelected;
+  final void Function()? onDelete;
+  final void Function()? onTap;
+  final List trashCan;
+
+  const PrettyCard(
+      {Key? key,
+      required this.name,
+      required this.isSelected,
+      this.onDelete,
+      this.onTap,
+      required this.trashCan})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+      child: Card(
+        surfaceTintColor: isSelected ? Colors.black : null,
+        color: Colors.cyan.shade900,
+        child: ListTile(
+          dense: true,
+          selected: isSelected,
+          onTap: trashCan.isNotEmpty ? onDelete : onTap,
+          onLongPress: onDelete,
+          tileColor: Colors.cyan.shade900,
+          selectedColor: Colors.white,
+          selectedTileColor: Colors.cyan.shade900,
+          title: Text(name),
+          shape: RoundedRectangleBorder(
+              side: BorderSide(
+                  color: Colors.cyan.shade50,
+                  width: 3,
+                  style: isSelected ? BorderStyle.solid : BorderStyle.none)),
+        ),
+      ),
+    );
+  }
+}
+
+class Course {
+  static bool selected = false;
+  final String id;
+  final String name;
+  Course({
+    required this.id,
+    required this.name,
+  });
+}
+
+List<Course> coursesData = [
+  Course(id: '1', name: 'MTH'),
+  Course(id: '2', name: 'STS'),
+  Course(id: '3', name: 'ACC'),
+  Course(id: '4', name: 'ETH'),
+  Course(id: '5', name: 'PHY'),
+  Course(id: '6', name: 'CSC'),
+];
